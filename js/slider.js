35ff024869699b9e76f36e9a3ce55854037b5f86
Emman1414 <0321-2818@lspu.edu.ph>
2024-10-03 23:49:10 +0800

    adasda

---
+++ js/slider.js
@@ -0,0 +1,62 @@
+var slider = tns({
+  container: ".banner__slider",
+  items: 1,
+  speed: 300,
+  slideBy: "page",
+  autoplay: true,
+  nav: true, // Navigation dots enabled
+  navPosition: "bottom", // Position at the bottom
+  controls: false,
+  responsive: {
+    1250: {
+      items: 1,
+    },
+    200: {
+      items: 1,
+    },
+  },
+});
+
+// var slider = tns({
+//   container: ".services__slider",
+//   items: 3,
+//   speed: 200,
+//   gutter: 100,
+//   slideBy: "page ",
+//   autoplay: true,
+//   navPosition: "bottom",
+//   // containerControls: "#controls",
+//   // prevButton: ".prev",
+//   // nextButton: ".next",
+//   responsive: {
+//     1250: {
+//       items: 1,
+//     },
+//     200: {
+//       items: 1,
+//     },
+//   },
+// });
+
+var serviceSlider = tns({
+  container: ".services__slider",
+  items: 3, // Display 3 items by default
+  slideBy: "page",
+  autoplay: false,
+  controlsContainer: "#controls",
+  prevButton: ".prev",
+  nextButton: ".next",
+  nav: false, // Disable nav dots if unnecessary
+  speed: 300,
+  responsive: {
+    1024: {
+      items: 3, // Show 3 items on screens wider than 1024px
+    },
+    768: {
+      items: 2, // Show 2 items on tablet-sized screens
+    },
+    480: {
+      items: 1, // Show 1 item on mobile screens
+    },
+  },
+});
